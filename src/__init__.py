863fbebf781415ca23f58d2dcf39b6c103ac3505
Kevin Lee <stickems94@gmail.com>
2017-05-02 21:28:56 -0700

    small changes to search. some refactoring

--- src/__init__.py
+++ src/__init__.py
@@ -7,9 +7,8 @@ class DegreesOfSeparation:
         self.__sp = spotipy.Spotify()
 
     def main(self):
-        beginning_artist, ending_artist = "chance the rapper", "kendrick lamar"  #retrieve_artists()
-        initial_artist_graph = self.get_initial_artist_graph(beginning_artist, ending_artist)
-        result = self.breadth_first_search(initial_artist_graph, beginning_artist, ending_artist)
+        beginning_artist, ending_artist = "Chance the Rapper", "Kendrick Lamar"  #retrieve_artists()
+        result = self.perform_artist_search(beginning_artist, ending_artist)
         print(result)
 
     def retrieve_artists(self):
@@ -17,52 +16,55 @@ class DegreesOfSeparation:
         ending_artist = input("Name of artist to end search: ").lower()
         return beginning_artist, ending_artist
 
-    def get_initial_artist_graph(self, beginning_artist, ending_artist):
+    def perform_artist_search(self, beginning_artist, ending_artist):
         featured_artists = {beginning_artist: set([])}
 
         results = self.__sp.search(q=beginning_artist, limit=50)
         for track in results['tracks']['items']:
             for artist in track['artists']:
-                featured_artist = artist['name'].lower()
+                featured_artist = artist['name']
 
                 if featured_artist == ending_artist:
-                    print(track['name'])
-                    return
+                    return [beginning_artist, ending_artist]
 
                 if featured_artist not in featured_artists:
                     featured_artists[beginning_artist].add(featured_artist)
 
-        return featured_artists
+        return self.artist_subsearch(featured_artists, beginning_artist, ending_artist)
 
-    def breadth_first_search(self, artist_graph, starting_artist, ending_artist):
+    def artist_subsearch(self, artist_graph, starting_artist, ending_artist):
         queue = [(starting_artist, [starting_artist])]
         # iterate through queue and pick up the first elements
         while queue:
             (vertex, path) = queue.pop(0)
             # find next unique artist in associated artists graph
             not_yet_visited = artist_graph[vertex] - set(path)
-            for next_artist in not_yet_visited:
 
-                artists = artist_graph.get(next_artist, set([]))
+            for current_artist in not_yet_visited:
+                featured_artists = self.search_for_featured_artists(artist_graph, current_artist, ending_artist, path)
+                artist_graph[current_artist] = featured_artists
 
-                results = self.__sp.search(q=next_artist, limit=50)
-                for track in results['tracks']['items']:
-                    if len(track['artists']) > 1:
-                        for artist in track['artists']:
-                            featured_artist = artist['name'].lower()
-
-                            if featured_artist == ending_artist:
-                                return path + [next_artist, featured_artist]
-
-                            if featured_artist not in artists:
-                                artists.add(featured_artist)
-
-                artist_graph[next_artist] = artists
-
-                if next_artist == ending_artist:
-                    return path + [next_artist]
+                if featured_artists is not None and ending_artist in featured_artists:
+                    return featured_artists
+                elif current_artist == ending_artist:
+                    return path + [current_artist]
                 else:
-                    queue.append((next_artist, path + [next_artist]))
+                    queue.append((current_artist, path + [current_artist]))
+
+    def search_for_featured_artists(self, artist_graph, initial_artist, ending_artist, path):
+        track_artists = artist_graph.get(initial_artist, set([]))
+
+        results = self.__sp.search(q=initial_artist, limit=50)
+        for track in results['tracks']['items']:
+            if len(track['artists']) > 1:
+                for artist in track['artists']:
+                    featured_artist = artist['name']
+
+                    if str(featured_artist).lower() == str(ending_artist).lower():
+                        return path + [initial_artist, featured_artist]
+
+                    if featured_artist not in track_artists:
+                        track_artists.add(featured_artist)
 
 
 degrees_of_separation = DegreesOfSeparation()
